7041510868217523a6b6da83f2124ef0d9e3af26
galvin-SH <mgalvin1993@gmail.com>
2023-11-20 18:47:42 -0800

    added apollo provider to app.jsx and updated SearchBooks.jsx to use the apollo defined mutation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <>
+                <Navbar />
+                <Outlet />
+            </>
+        </ApolloProvider>
+    );
 }
 
 export default App;
